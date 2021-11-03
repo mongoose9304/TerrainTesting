63669f1faa7fb15f99ba8bcc2106e6dad4d5e54f
Ryan Thorne <rymaster_109@hotmail.com>
2021-11-03 17:41:02 -0400

    set up unity and asset

---
+++ TerrainTesting/Assets/TerrainGridSystem/Demos/Demo03_CellsVisibility/Demo3.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TGS {
+	public class Demo3 : MonoBehaviour {
+
+		TerrainGridSystem tgs;
+		GUIStyle labelStyle;
+
+		void Start () {
+			// setup GUI styles
+			labelStyle = new GUIStyle ();
+			labelStyle.alignment = TextAnchor.MiddleCenter;
+			labelStyle.normal.textColor = Color.black;
+
+			tgs = TerrainGridSystem.instance;
+
+			// hide all cells
+			for (int k = 0; k < tgs.cells.Count; k++) {
+				tgs.CellSetBorderVisible (k, false);
+			}
+			tgs.Redraw(); // forces redraw immediately
+
+            // listen to events
+            tgs.OnCellClick += OnCellClick;
+            tgs.OnCellDragStart += OnCellDragStart;
+            tgs.OnCellDrag += OnCellDrag;
+            tgs.OnCellDragEnd += OnCellDragEnd;
+
+		}
+
+		private void OnCellClick(TerrainGridSystem sender, int cellIndex, int buttonIndex) {
+			// CellSetBorderVisible controls the visibility of the cell's border
+			// CellSetVisible controls the visibility of both cell's border and surface
+			tgs.CellSetBorderVisible(cellIndex, !tgs.CellHasBorderVisible(cellIndex));
+		}
+
+		private void OnCellDragEnd(TerrainGridSystem sender, int cellOriginIndex, int cellTargetIndex) {
+			Debug.Log("Drags ends on " + cellTargetIndex);
+        }
+
+        private void OnCellDrag(TerrainGridSystem sender, int cellOriginIndex, int cellTargetIndex) {
+			Debug.Log("Dragging cell " + cellOriginIndex + " over " + cellTargetIndex);
+		}
+
+		private void OnCellDragStart(TerrainGridSystem sender, int cellIndex) {
+			Debug.Log("Drag starts on cell " + cellIndex);
+		}
+
+
+		void OnGUI () {
+			GUI.Label (new Rect (0, 5, Screen.width, 30), "Click on any position to toggle cell visibility.", labelStyle);
+		}
+
+
+	}
+}
---
+++ TerrainTesting/Assets/TerrainGridSystem/Scripts/Core/Input/NewInputSystem.cs
@@ -0,0 +1,100 @@
+﻿#if ENABLE_INPUT_SYSTEM
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+namespace TGS {
+    /// <summary>
+    /// This class provides an input layer for the new input system that can be replaced or overriden to provide custom support
+    /// </summary>
+    public class NewInputSystem : IInputProxy {
+
+        public virtual void Init() {
+            UnityEngine.InputSystem.EnhancedTouch.EnhancedTouchSupport.Enable();
+        }
+
+        public virtual Vector3 mousePosition { get { return Mouse.current.position.ReadValue(); } }
+
+        public virtual bool touchSupported { get { return Touchscreen.current != null; } }
+
+        public virtual int touchCount { get { return UnityEngine.InputSystem.EnhancedTouch.Touch.activeTouches.Count; } }
+
+        public virtual LocationService location { get { return null; } }
+
+        public virtual bool GetMouseButton(int buttonIndex) {
+            switch (buttonIndex) {
+                case 1: return Mouse.current.rightButton.isPressed;
+                case 2: return Mouse.current.middleButton.isPressed;
+                default: return Mouse.current.leftButton.isPressed;
+            }
+        }
+
+        public virtual bool GetMouseButtonDown(int buttonIndex) {
+            switch (buttonIndex) {
+                case 1: return Mouse.current.rightButton.wasPressedThisFrame;
+                case 2: return Mouse.current.middleButton.wasPressedThisFrame;
+                default: return Mouse.current.leftButton.wasPressedThisFrame;
+            }
+        }
+
+        public virtual bool GetMouseButtonUp(int buttonIndex) {
+            switch (buttonIndex) {
+                case 1: return Mouse.current.rightButton.wasReleasedThisFrame;
+                case 2: return Mouse.current.middleButton.wasReleasedThisFrame;
+                default: return Mouse.current.leftButton.wasReleasedThisFrame;
+            }
+        }
+
+        public virtual bool IsTouchStarting(int touchIndex) {
+            UnityEngine.InputSystem.EnhancedTouch.Touch touch = UnityEngine.InputSystem.EnhancedTouch.Touch.activeTouches[touchIndex];
+            return touch.phase == UnityEngine.InputSystem.TouchPhase.Began;
+        }
+
+        public virtual int GetFingerIdFromTouch(int touchIndex) {
+            UnityEngine.InputSystem.EnhancedTouch.Touch touch = UnityEngine.InputSystem.EnhancedTouch.Touch.activeTouches[touchIndex];
+            return touch.finger.index;
+        }
+
+        public virtual bool GetKey(string name) {
+            return ((KeyControl)Keyboard.current[name]).isPressed;
+        }
+
+        public virtual bool GetKeyDown(string name) {
+            return ((KeyControl)Keyboard.current[name]).wasPressedThisFrame;
+        }
+
+        public virtual bool GetKeyUp(string name) {
+            return ((KeyControl)Keyboard.current[name]).wasReleasedThisFrame;
+        }
+
+
+        // Note: the followihg methods are not implemented. Feel free to add your own input 
+        public virtual float GetAxis(string axisName) {
+            return 0;
+        }
+
+        public virtual bool GetButtonDown(string buttonName) {
+            return false;
+        }
+
+        public virtual bool GetButtonUp(string buttonName) {
+            return false;
+        }
+
+        public virtual bool GetKey(KeyCode keyCode) {
+            return false;
+        }
+
+        public virtual bool GetKeyDown(KeyCode keyCode) {
+            return false;
+        }
+
+        public virtual bool GetKeyUp(KeyCode keyCode) {
+            return false;
+        }
+
+    }
+}
+
+#endif
